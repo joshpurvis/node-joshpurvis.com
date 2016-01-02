513b80384b00888023b35862bc476980a8965735
Josh Purvis <jpurvis@kwantera.com>
2016-01-02 15:33:39 -0500

    remove unused dependencies and shrinkwrap

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var nunjucks = require('nunjucks');
 
@@ -17,9 +16,6 @@ nunjucks.configure('views', {
   express: app
 });
 app.set('view engine', 'html');
-
-// favicon
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 // middleware
 app.use(logger('dev'));
